0abbb2a1c8dd5bad6e5f4f1e8d9b0ba58cf0769c
J. Cravens <jaycrav3ns@gmail.com>
2025-03-22 16:26:52 -0500

    Add files via upload

--- modules/reporter.py
+++ modules/reporter.py
@@ -1,14 +1,22 @@
 #!/usr/bin/env python3
 
 from jinja2 import Template
+import json
 
 class HTMLreporter:
     def __init__(self, templateFileName="templates/report.htm"):
-        with open(templateFileName, encoding="utf-8") as templateFile:  # Specify encoding for reading
+        with open(templateFileName, encoding="utf-8") as templateFile:
             self._template = Template(templateFile.read())
         
     def report(self, rows, reportFileName=None):
         if reportFileName is None:
             reportFileName = "output.html"
-        with open(reportFileName, "wt", encoding="utf-8") as reportFile:  # Specify encoding for writing
-            reportFile.write(self._template.render(rows=rows))  # Write the string directly
+        with open(reportFileName, "wt", encoding="utf-8") as reportFile:
+            reportFile.write(self._template.render(rows=rows))
+
+class JSONreporter:
+    def report(self, rows, reportFileName=None):
+        if reportFileName is None:
+            reportFileName = "output.json"
+        with open(reportFileName, "w", encoding="utf-8") as reportFile:
+            json.dump(rows, reportFile, ensure_ascii=False, indent=2)
